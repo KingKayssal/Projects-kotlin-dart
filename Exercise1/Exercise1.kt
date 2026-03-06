c1d72056bbd2f96a57b9eefc9a814d9704f9fd3f
kingFayssal <ndanjofa@gmail.com>
2026-03-06 04:42:47 +0100

    assigment1

--- Exercise1/Exercise1.kt
+++ Exercise1/Exercise1.kt
@@ -2,19 +2,15 @@ fun main() {
 
     val nums = listOf(1, 2, 3, 4, 5, 6)
 
-    // Test 1: Get even numbers
     val even = processList(nums) { it % 2 == 0 }
     println("Even numbers:   $even")
-
-    // Test 2: Get numbers greater than 3
+    
     val greaterThan3 = processList(nums) { it > 3 }
     println("Greater than 3: $greaterThan3")
 
-    // Test 3: Get odd numbers
     val odd = processList(nums) { it % 2 != 0 }
     println("Odd numbers:    $odd")
 
-    // Test 4: Get numbers less than 4
     val lessThan4 = processList(nums) { it < 4 }
     println("Less than 4:    $lessThan4")
 }
@@ -33,4 +29,5 @@ fun processList(
     }
 
     return result
+
 }
